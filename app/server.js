ad0901d7f3d5e0f0de8344744d0d9eef4e57c45c
Denis Hilt <dhilt.public@gmail.com>
2023-06-04 02:15:40 +0200

    demo, runner

--- app/server.js
+++ app/server.js
@@ -66,12 +66,25 @@ app.use(express.static(path.join(__dirname, './static')));
 const templates = new Map();
 const TEMPLATE_DEFAULT = './templates/common.html';
 
-app.get('/*', (req, res) => {
-  console.log(req.originalUrl);
-  const conf = CONF.find(c => c.route === req.originalUrl);
-  if (!conf) {
-    return res.sendStatus(404);
+const runners = new Map();
+const RUNNER_DEFAULT = './runners/common.js';
+
+const loadEntity = ({ key, map, file }) => {
+  let result = map.get(key);
+  if (!result) {
+    try {
+      const templatePath = path.join(__dirname, './static', file);
+      result = readFileSync(templatePath, { encoding: 'utf8' });
+      map.get(key, result);
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
   }
+  return result;
+};
+
+const makeHtml = ({ conf }) => {
   let html = demoHtml
     .replace('<!-- head-title -->', ` ${conf.title.head}`)
     .replace('<!-- body-title -->', ` ${conf.title.body}`);
@@ -79,18 +92,25 @@ app.get('/*', (req, res) => {
     html = html.replace('<!-- body-description -->', ` ${conf.description}`);
   }
 
-  let template = templates.get(conf.route);
+  const template = loadEntity({
+    key: conf.route,
+    map: templates,
+    file: conf.template || TEMPLATE_DEFAULT
+  });
   if (!template) {
-    try {
-      const templatePath = path.join(__dirname, './static', conf.template || TEMPLATE_DEFAULT);
-      template = readFileSync(templatePath, { encoding: 'utf8' });
-      templates.get(conf.route, template);
-    } catch (err) {
-      console.log(err);
-      return res.sendStatus(400);
-    }
+    return false;
   }
   html = html.replace('<!-- template -->', ` ${template}`);
+
+  const runner = loadEntity({
+    key: conf.route,
+    map: runners,
+    file: conf.runner || RUNNER_DEFAULT
+  });
+  if (!runner) {
+    return false;
+  }
+  html = html.replace('<!-- runner -->', `<script>${runner}</script>`);
 
   if (conf.ds) {
     let dsScript = `<script src="${conf.ds}"></script>`;
@@ -98,6 +118,19 @@ app.get('/*', (req, res) => {
       dsScript += '<script>window.__vscroll__.needRun = true</script>';
     }
     html = html.replace('<!-- datasource -->', dsScript);
+  }
+  return html;
+};
+
+app.get('/*', (req, res) => {
+  console.log(req.originalUrl);
+  const conf = CONF.find(c => c.route === req.originalUrl);
+  if (!conf) {
+    return res.sendStatus(404);
+  }
+  const html = makeHtml({ conf });
+  if (!html) {
+    return res.sendStatus(400);
   }
   return res.send(html);
 });

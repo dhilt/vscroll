a8665f97a9378b9656ba76df2d8a693d864c1143
Denis Aleksanov <daleksanov@nvidia.com>
2026-07-24 22:32:49 +0200

    issue-66 e2e v3, dependencies

--- tests/e2e/vitest.config.ts
+++ tests/e2e/vitest.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   },
   test: {
     include: ['e2e/specs/**/*.spec.ts'],
-    fileParallelism: false,
+    fileParallelism: process.env.CI === 'true',
     attachmentsDir: '.vitest-attachments',
     globals: true,
     testTimeout: 5000,
